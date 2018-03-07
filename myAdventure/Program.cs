4b041d33bcc723add28bbac6b45ec05a21d7283a
Preston Moore <moorepjay@gmail.com>
2018-03-07 00:51:55 -0500

    Update.

--- myAdventure/Program.cs
+++ myAdventure/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace myAdventure
 {
@@ -10,6 +6,60 @@ namespace myAdventure
     {
         static void Main(string[] args)
         {
+            Hero myHero = new Hero("Alouris");
+            Monster myMonster = new Monster("Goblin");
+            myHero.HeroCreated();
+            Console.WriteLine("A {0} appears!", myMonster.Species);
+
+            myHero.TakeDamage(3);
         }
     }
+
+    class Hero
+    {
+        // Fields
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+
+        // Constructor
+        public Hero(string HeroName)
+        {
+            Name = HeroName;
+            Health = 10;
+            Damage = 2;
+        }
+
+        // Method
+        public void HeroCreated()
+        {
+            Console.WriteLine("A new Hero joins the fight!");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Health: " + Health);
+            Console.WriteLine("Damage: " + Damage);
+        }
+
+        public void TakeDamage(int incomingDamage)
+        {
+            Health -= incomingDamage;
+            Console.WriteLine(Health);
+        }
+    }
+
+    class Monster
+    // Look into inheritance.
+    {
+        public string Species { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+
+        public Monster(string species)
+        {
+            Species = species;
+            Health = 5;
+            Damage = 1;
+        }
+
+    }
+    
 }
